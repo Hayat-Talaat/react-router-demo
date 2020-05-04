ff110256ca02921a8b142268c675b7802bb01e12
Hayat Talaat <admin@Admins-MacBook-Pro.local>
2020-05-04 19:52:28 +0200

    build

---
+++ build/precache-manifest.8de7e5016f2e9f28a67eb2442ce754c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a038af3b401bd7ac4688f4059f12b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4862d7453164c84bfd9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40a297deb7701026ab4f",
+    "url": "/static/js/2.422324cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.422324cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4862d7453164c84bfd9",
+    "url": "/static/js/main.36a65783.chunk.js"
+  },
+  {
+    "revision": "8c662f6c7f56ee3546cd",
+    "url": "/static/js/runtime-main.3fb767b5.js"
+  }
+]);
